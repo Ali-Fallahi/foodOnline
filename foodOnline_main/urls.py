05c51677ce61b0076792c046ba081eb9bc27073c
Ali-Fallahi <fallahia80@gmail.com>
2023-06-20 00:32:03 +0330

    Place order and generate order number

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -34,5 +34,7 @@ urlpatterns = [
                   path('search/', MarketplaceViews.search, name='search'),
                   # CHECKOUT
                   path('checkout/', MarketplaceViews.checkout, name='checkout'),
+                  # ORDERS
+                  path('orders/', include('orders.urls'))
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ orders/utils.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+def generate_order_number(pk):
+    current_datetime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    order_number = current_datetime + str(pk)
+    return order_number
